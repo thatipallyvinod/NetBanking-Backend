8877da06716dc9daf3d23d9ad78f3ac94e2dab02
thatipallyvinod <thatipallyvinod@gmail.com>
2024-07-12 10:57:52 +0530

    add

--- src/main/java/com/bankingapp/model/Account.java
+++ src/main/java/com/bankingapp/model/Account.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 
 import jakarta.persistence.CascadeType;
-
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -15,6 +15,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
